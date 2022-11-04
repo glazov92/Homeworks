2449e6faeab5180a5778309d0f726bce8013cd69
glazov92 <116938493+glazov92@users.noreply.github.com>
2022-11-05 02:57:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// Задание 1
+console.log('Hello world!');
+// Задание 2
+const age = 23;
+let userName ='Вася';
+let bool = true;
+let nameNull = null;
+let undef;
+let obj= {first:1, second:2};
+let id = Symbol("id");
+let bigInt = 1321231327456894n
+// Задание 3
+console.log(age);
+console.log(userName);
+console.log(bool);
+console.log(nameNull);
+console.log(undef);
+console.log(obj);
+console.log(id);
+console.log(bigInt);
+// Задание 4
+console.log(typeof age);
+console.log(typeof userName);
+console.log(typeof bool);
+console.log(typeof nameNull);
+console.log(typeof undef);
+console.log(typeof obj);
+console.log(typeof id);
+console.log(typeof bigInt);
+// Задание 5
+//console.log(age=25);// не получается 😭
+/*const objct= {first:3, second:4};
+console.log(objct) 
+objct= {first:5, second:6};
+console.log(objct)   не получается 😭 */
+// Задание 7
+userName="vasya"
+console.log(userName);
+// Задание 8
+var catNames={first:"Pushok", second:"Рыжий"};
+console.log(catNames);
+catNames={first:"Пушок", second:"Rijiy"};
+console.log(catNames);
